0364d861c1bea34d9d3c5fc66312802982596ab7
jim-ww <jim.w2610@proton.me>
2026-08-25 17:16:57 +0200

    add flake.nix packaging bdraw with buildGoModule

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "bdraw - a mouse-first paint program for the terminal";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.buildGoModule {
+          pname = "bdraw";
+          version = "0.1.0";
+          src = ./.;
+
+          vendorHash = "sha256-UsvPBIPO8cLVmFbZUlueAuiMd5fEGSi6Omf6jTr64Wo=";
+
+          meta = with pkgs.lib; {
+            description = "A mouse-first paint program for the terminal";
+            homepage = "https://github.com/jim-ww/bdraw";
+            license = licenses.mit;
+            mainProgram = "bdraw";
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ pkgs.go ];
+        };
+      });
+}
